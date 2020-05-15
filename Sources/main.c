d1e07dde1e3b91009ebaa9d24b2b1ab970ca4e14
Jack Kilby <jiakai@thirdreality.ai>
2020-05-15 08:45:48 +0800

    [Third Project][Task 5] 2 Leds blink with 2 Buttons. [Bug] Button jitter.

--- Sources/main.c
+++ Sources/main.c
@@ -22,6 +22,7 @@ void delay(void)
 int main(void)
 {
 	int counter = 0;
+	unsigned char mode = 1;
 	
 	//PortC Clock Gating
 	SIM_SCGC5 |= (1<<11);
@@ -31,32 +32,47 @@ int main(void)
 	//Select MUX feature as GPIO for PortC-GPIO5(LED1-Blue) and PortC-GPIO0(LED2-Green)
 	PORTC_PCR5 |= 0x100;
 	PORTC_PCR0 |= 0x100;
-	//Select MUX feature as GPIO for PortA-GPIO5(Button-A) and PortA-GPIO12(Button-B)
+	//Select MUX feature as GPIO for PortA-GPIO5(Button-A) and PortA-GPIO12(Button-B) and PortA-GPIO19(Button-C)
 	PORTA_PCR5 |= 0x100;
 	PORTA_PCR12 |= 0x100;
+	PORTA_PCR19 |= 0x100;
 	
 	//Init PortC-GPIO5(LED1-Blue) and PortC-GPIO0(LED2-Green) as output LOW
 	GPIOC_PDDR |= (1<<5) | (1<<0);
 	GPIOC_PDOR &= ~(1<<5);
 	GPIOC_PDOR |= (1<<0);
-	//Init PortA-GPIO5(Button-A) and PortA-GPIO12(Button-B) as Input Pullup
+	//Init PortA-GPIO5(Button-A) and PortA-GPIO12(Button-B) and PortA-GPIO19(Button-C) as Input Pullup
 	PORTA_PCR5 |= (1<<1) | (1<<0);
-	PORTA_PCR12 |= (1<<1) | (1<<0);	
+	PORTA_PCR12 |= (1<<1) | (1<<0);
+	PORTA_PCR19 |= (1<<1) | (1<<0);
 
 	for(;;) {	   
 	   	counter++;
 //	   	delay();
-	   	//Button-A control PortC-GPIO5(LED1-Blue)
-	   	if (!(GPIOA_PDIR & (1<<5))) {
-	   		GPIOC_PDOR |= (1<<5);
+	   	//Select mode
+	   	if (!(GPIOA_PDIR & (1<<19))) {
+	   		mode = !mode;
+	   	} 
+	   	
+	   	if (mode) {
+	   		//Mode-1
+	   		if (!(GPIOA_PDIR & (1<<5)) || !(GPIOA_PDIR & (1<<12))) {
+				GPIOC_PTOR |= (1<<5) | (1<<0);
+			}
 	   	} else {
-	   		GPIOC_PDOR &= ~(1<<5);
-	   	}
-	   	//Button-B control PortC-GPIO0(LED2-Green)
-	   	if (!(GPIOA_PDIR & (1<<12))) {
-	   		GPIOC_PDOR  |= (1<<0);
-	   	} else {
-	   		GPIOC_PDOR  &= ~(1<<0);
+			//Mode-2
+			//Button-A control PortC-GPIO5(LED1-Blue)
+			if (!(GPIOA_PDIR & (1<<5))) {
+				GPIOC_PDOR |= (1<<5);
+			} else {
+				GPIOC_PDOR &= ~(1<<5);
+			}
+			//Button-B control PortC-GPIO0(LED2-Green)
+			if (!(GPIOA_PDIR & (1<<12))) {
+				GPIOC_PDOR |= (1<<0);
+			} else {
+				GPIOC_PDOR &= ~(1<<0);
+			}
 	   	}
 	}
 	
