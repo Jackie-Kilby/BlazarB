379625960ee5b362350d8c01a61794dd1e3c9d82
Jack Kilby <jiakai@thirdreality.ai>
2020-05-16 23:30:00 +0800

    [GPIO Func] Refactor the code and make it compatible with the 3 LEDs and 8 Buttons.

--- Sources/main.c
+++ Sources/main.c
@@ -1,119 +1,215 @@
 /*
- * main implementation: use this 'C' sample to create your own application
- *
- */
+File name		: main.c
+Developer		: Jack Kilby
+First Bread		: 2020-05-16
+Description		: The main entry of the project.
+*/
 
+/************************ Include Files ***********************************************/
 #include "derivative.h" /* include peripheral declarations */
+#include <stdio.h>
 
-#define JITTER_CNT	1
+/************************ MACRO Definitions *******************************************/
 #define PERIOD		100
 
+/************************ Type Definitions ********************************************/
 
-void delay(int delay_time) {
-	unsigned short i, j;
-	for (i = 0; i < 30; i++) {
-		for (j = 0; j < delay_time; j++) {
-//			asm{"nop"};
-		}
-	}
+/************************ Externed Varibles *******************************************/
+
+/************************ Static Varibles *********************************************/
+
+/************************ Static Functions Prototypes *********************************/
+void delay(int delay_cnt);
+void led_init(void);
+void button_init(void);
+void led_blink_mode_normal_blink(int on_cnt, int off_cnt);
+void led_blink_mode_breath_blink(void);
+
+/************************ Externed Functions Definitions ******************************/
+int button_read(void)
+{
+	int button_ret = 0;
+	
+	if ( !(GPIOC_PDIR & (1<<9)) )
+		button_ret |= (1<<0);
+	if ( !(GPIOC_PDIR & (1<<7)) )
+		button_ret |= (1<<1);
+	if ( !(GPIOC_PDIR & (1<<8)) )
+		button_ret |= (1<<2);
+	if ( !(GPIOC_PDIR & (1<<6)) )
+		button_ret |= (1<<3);
+	if ( !(GPIOA_PDIR & (1<<5)) )
+		button_ret |= (1<<4);
+	if ( !(GPIOA_PDIR & (1<<12)) )
+		button_ret |= (1<<5);
+	if ( !(GPIOA_PDIR & (1<<19)) )
+		button_ret |= (1<<6);
+	if ( !(GPIOA_PDIR & (1<<13)) )
+		button_ret |= (1<<7);
+	
+	return button_ret;
 }
-
-int main(void) {
-	int counter = 0;
-	int on_time = 300;
-
-	//PortC Clock Gating
-	SIM_SCGC5 |= (1 << 11);
-	//PortA Clock Gating
-	SIM_SCGC5 |= (1 << 9);
-
-	//Select MUX feature as GPIO for PortC-GPIO5(LED1-Blue) and PortC-GPIO0(LED2-Green)
-	PORTC_PCR5 |= 0x100;
-	PORTC_PCR0 |= 0x100;
-	//Select MUX feature as GPIO for PortA-GPIO5(Button-A) and PortA-GPIO12(Button-B) and PortA-GPIO19(Button-C)
-	PORTA_PCR5 |= 0x100;
-	PORTA_PCR12 |= 0x100;
-	PORTA_PCR19 |= 0x100;
-
-	//Init PortC-GPIO5(LED1-Blue) and PortC-GPIO0(LED2-Green) as output LOW
-	GPIOC_PDDR |= (1 << 5) | (1 << 0);
-	GPIOC_PDOR &= ~(1 << 5);
-	GPIOC_PDOR |= (1 << 0);
-	//Init PortA-GPIO5(Button-A) and PortA-GPIO12(Button-B) and PortA-GPIO19(Button-C) as Input Pullup
-	PORTA_PCR5 |= (1<<1) | (1<<0);
-	PORTA_PCR12 |= (1<<1) | (1<<0);
-	PORTA_PCR19 |= (1<<1) | (1<<0);
-
+/*************************************************************************
+* Function Name		:	main
+* Description		:	The User Program Entry
+* Parameters		:	void
+* Returns			:	int
+*************************************************************************/
+int main(void) 
+{
+	int button_st = 0;
+	
+	led_init();
+	button_init();
+	
 	for (;;) {
-		counter++;
-		//Breathing light
-		//Step 1
-		for (on_time=0 ; on_time<PERIOD ; on_time++) {
-			//on
-			GPIOC_PDOR |= (1<<5);
-			//delay++
-			delay(on_time);
-			//off
-			GPIOC_PDOR &= ~(1<<5);
-			//delay--
-			delay(PERIOD-on_time);
-		}
-		for (on_time=0 ; on_time<PERIOD ; on_time++) {
-			//on
-			GPIOC_PDOR |= (1<<5);
-			//delay++
-			delay(PERIOD-on_time);
-			//off
-			GPIOC_PDOR &= ~(1<<5);
-			//delay--
-			delay(on_time);
-		}
-		GPIOC_PTOR |= (1<<0);
+		button_st = button_read();
+		if ( button_st & (1<<3))
+			led_blink_mode_breath_blink();
 	}
 
 	return 0;
 }
 
-//Blink with delay
-void led_blink_mode_1(void)					
+/************************ Static Functions Definitions *********************************/
+/*************************************************************************
+* Function Name		:	led_init
+* Description		:	Initilize LED GPIOs
+* Parameters		:	void
+* Returns			:	void
+*************************************************************************/
+void led_init(void)
 {
-	delay(200);
-	GPIOC_PTOR |= (1<<5) | (1<<0);
+	/*
+	 * Led List:	
+	 * 				D4			D7			D8
+	 * Red:			PortC-3		PortB-19	PortB-16
+	 * Green:		PortC-4		PortC-0		PortB-17
+	 * Blue:		PortC-5		PortC-2		PortB-18
+	 */
+	//Enable Port Clock gate
+	SIM_SCGC5 |= (1<<10) | (1<<11);
+	//Select MUX as GPIO
+	PORTC_PCR3 |= 0x100;
+	PORTC_PCR4 |= 0x100;
+	PORTC_PCR5 |= 0x100;
+	PORTB_PCR19 |= 0x100;
+	PORTC_PCR0 |= 0x100;
+	PORTC_PCR2 |= 0x100;
+	PORTB_PCR16 |= 0x100;
+	PORTB_PCR17 |= 0x100;
+	PORTB_PCR18 |= 0x100;
+	//Init GPIO as output
+	GPIOC_PDDR |= (1<<3) | (1<<4) | (1<<5) | (1<<0) | (1<<2);
+	GPIOB_PDDR |= (1<<19) | (1<<16) | (1<<17) | (1<<18);
+	//Init GPIO as LOW
+	GPIOC_PDOR &= ~( (1<<3) | (1<<4) | (1<<5) | (1<<0) | (1<<2) );
+	GPIOB_PDOR &= ~( (1<<19) | (1<<16) | (1<<17) | (1<<18) );
 }
 
-//Button press -> LED on.
-void led_blink_mode_2(void)
+/*************************************************************************
+* Function Name		:	button_init
+* Description		:	Initilize Button GPIOs
+* Parameters		:	void
+* Returns			:	void
+*************************************************************************/
+void button_init(void)
 {
-	static int jitterA = 0, jitterB = 0;
-	
-	//Button-A control PortC-GPIO5(LED1-Blue)
-	if (!(GPIOA_PDIR & (1 << 5))) {
-		jitterA++;
-		if (jitterA == JITTER_CNT) {
-			GPIOC_PDOR |= (1 << 5);
-		}
-	} else {
-		GPIOC_PDOR &= ~(1 << 5);
-		if (jitterA) {
-			jitterA = 0;
-		}
-	}
-	//Button-B control PortC-GPIO0(LED2-Green)
-	if (!(GPIOA_PDIR & (1 << 12))) {
-		jitterB ++;
-		if (jitterB == JITTER_CNT) {
-			GPIOC_PDOR |= (1 << 0);
-		}
-	} else {
-		GPIOC_PDOR &= ~(1 << 0);
-		if (jitterB) {
-			jitterB = 0;
+	/*
+	 * Button List:
+	 * 				UP			DOWN		LEFT		RIGHT
+	 * Direction	PortC-9		PortC-7		PortC-8		PortC-6
+	 * 				A			B			C			D
+	 * Function		PortA-5		PortA-12	PortA-19	PortA-13
+	 */
+	//Enable Port Clock gate
+	SIM_SCGC5 |= (1<<9) | (1<<11);
+	//Select MUX as GPIO
+	PORTC_PCR9 |= 0x100;
+	PORTC_PCR7 |= 0x100;
+	PORTC_PCR8 |= 0x100;
+	PORTC_PCR6 |= 0x100;
+	PORTA_PCR5 |= 0x100;
+	PORTA_PCR12 |= 0x100;
+	PORTA_PCR19 |= 0x100;
+	PORTA_PCR13 |= 0x100;
+	//Init GPIO as input
+	GPIOC_PDDR &= ~( (1<<9) | (1<<7) | (1<<8) | (1<<6) );
+	GPIOA_PDDR &= ~( (1<<5) | (1<<12) | (1<<19) | (1<<13) );
+	//Init GPIO as pullup
+	PORTC_PCR9 |= (1<<1) | (1<<0);
+	PORTC_PCR7 |= (1<<1) | (1<<0);
+	PORTC_PCR8 |= (1<<1) | (1<<0);
+	PORTC_PCR6 |= (1<<1) | (1<<0);
+	PORTA_PCR5 |= (1<<1) | (1<<0);
+	PORTA_PCR12 |= (1<<1) | (1<<0);
+	PORTA_PCR19 |= (1<<1) | (1<<0);
+	PORTA_PCR13 |= (1<<1) | (1<<0);
+}
+
+/*************************************************************************
+* Function Name		:	delay
+* Description		:	Used to delay a period of time, occupying CPU.
+* Parameters		:	int	delay_cnt, the cnt of the period.
+* Returns			:	void
+*************************************************************************/
+void delay(int delay_cnt) 
+{
+	unsigned short i, j;
+	for (i = 0; i < 30; i++) {
+		for (j = 0; j < delay_cnt; j++) {
+			asm("nop");
 		}
 	}
 }
 
-//Breathing Blink
-void led_blink_mode_3(void)
+/*************************************************************************
+* Function Name		:	led_blink_mode_normal_blink
+* Description		:	Normal blink with a certain on and off time
+* Parameters		:	int	on_cnt,	The on_cnt of Normal blink
+* 						int off_cnt, The off_cnt of Normal blink
+* Returns			:	void
+*************************************************************************/
+void led_blink_mode_normal_blink(int on_cnt, int off_cnt)
 {
-	
+	GPIOC_PDOR |= (1<<2) | (1<<0);
+	GPIOB_PDOR |= (1<<18);
+	delay(on_cnt);
+	GPIOC_PDOR &= ~( (1<<2) | (1<<0) );
+	GPIOB_PDOR &= ~(1<<18);
+	delay(off_cnt);
 }
+
+/*************************************************************************
+* Function Name		:	led_blink_mode_breath_blink
+* Description		:	Breath blink
+* Parameters		:	void
+* Returns			:	void
+*************************************************************************/
+void led_blink_mode_breath_blink(void)
+{
+	static int on_time = 0;
+	for (on_time=0 ; on_time<PERIOD ; on_time++) {
+		//on
+		GPIOC_PDOR |= (1<<5);
+		//delay++
+		delay(on_time);
+		//off
+		GPIOC_PDOR &= ~(1<<5);
+		//delay--
+		delay(PERIOD-on_time);
+	}
+	for (on_time=0 ; on_time<PERIOD ; on_time++) {
+		//on
+		GPIOC_PDOR |= (1<<5);
+		//delay++
+		delay(PERIOD-on_time);
+		//off
+		GPIOC_PDOR &= ~(1<<5);
+		//delay--
+		delay(on_time);
+	}
+}
+
+/* End of File*/
+
