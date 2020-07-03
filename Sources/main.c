69161ada16149bfa22b6a7839e0103a86934f3b9
Jack Kilby <jiakai@thirdreality.ai>
2020-07-03 23:32:11 +0800

    [Uart] Basic Uart Func.

--- Sources/main.c
+++ Sources/main.c
@@ -24,6 +24,35 @@ Description		: The main entry of the project.
 /************************ Static Functions Prototypes *********************************/
 
 /************************ Externed Functions Definitions ******************************/
+void UART0_PutChar(char c)
+{
+	while(!(UART0_S1 & UART_S1_TDRE_MASK));
+	UART0_D = c;
+}
+
+unsigned char UART0_GetChar(void)
+{
+	while(!(UART0_S1 & UART_S1_RDRF_MASK));
+	return UART0_D;
+}
+
+void UART0_Init(void)
+{
+	//Enable UART0 clock
+	SIM_SOPT2 |= (1<<26);			//MCGFLLCLK clock, or MCGPLLCLK/2
+	SIM_SOPT2 &= ~(1<<16);			//MCGFLLCLK clock
+	SIM_SCGC4 |= (1<<10);			//Enable UART0 clock
+	SIM_SCGC5 |= (1<<9);			//Enable PortA clock
+	
+	//Select UART0 IO
+	PORTA_PCR2 |= (1<<9);			//Select PortA-2 as UART0 Tx
+	PORTA_PCR1 |= (1<<9);			//Select PortA-1 as UART0 Rx
+	
+	//Init UART
+	UART0_BDH = 0x00;
+	UART0_BDL = 137;				//Set baud rate as 9600
+	UART0_C2 |= (UART_C2_TE_MASK | UART_C2_RE_MASK);			//Enable Tx
+}
 /*************************************************************************
 * Function Name		:	main
 * Description		:	The User Program Entry
@@ -32,15 +61,10 @@ Description		: The main entry of the project.
 *************************************************************************/
 int main(void) 
 {
-	int button_st = 0;
-	
-	led_init();
-	button_init();
+	UART0_Init();
 	
 	for (;;) {
-		button_st = button_read();
-		if ( button_st & (1<<3))
-			led_blink_mode_breath_blink();
+		UART0_PutChar('D');
 	}
 
 	return 0;
